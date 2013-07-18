3519f8adb2f8dc1044bf152a52b4edd2de42b4f8
Markuswu <markusskatermk@yahoo.com>
2013-07-18 20:15:39 +0000

    Edited product desc in index.html

--- grader.js
+++ grader.js
@@ -22,11 +22,26 @@ References:
 */
 
 var fs = require('fs');
+var util = require('util');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://floating-bayou-9904.herokuapp.com/";
 
+var checkUrl = function(url){
+    var y;
+    rest.get(url).on('complete', function(result, response){
+	if(result instanceof Error){
+	    console.error("error occurs in checkUrl");
+	}
+	else{
+	   y =  response.toString();
+	    return y;
+	}
+    });
+};
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
@@ -65,6 +80,7 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_file>', 'Path to myweb', clone(checkUrl), URL_DEFAULT)
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
